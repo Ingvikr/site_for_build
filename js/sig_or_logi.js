b1fe4ff3723b0e6fc3ebbda78bfb339511162b3b
igo <igoranatol870@gmail.com>
2022-05-20 16:43:43 +0300

    sdfsdf

--- js/sig_or_logi.js
+++ js/sig_or_logi.js
@@ -9,8 +9,17 @@ function log_or_sign () {
                                 us_usei1: {
                                     us_usenm1:"igo",
                                     us_usesr1:"romane",
-                                    us_useag1:"19"
+                                    us_useag1:"19",
+                                    us_useba1:"1390"
                                 }
+                            };
+
+                            var us_lodo1 = {
+                                us_dlh1:"13",
+                                us_dlh2:"15",
+                                us_dlh3:"11",
+                                us_dlh4:"19",
+                                us_dlh5:"17"
                             };
                             if(usi_log1.hasOwnProperty("us_usei1.us_usenm1") == true && usi_log1.us_usei1.us_usenm1 == "igo") {
                                 var us_lonm2;
@@ -37,6 +46,86 @@ function log_or_sign () {
         }
     }
     catch(e) {
+        if(confirm("Ваши свойства зарегестрирова")) {
+            if(confirm("Это платная функц")) {
+                if(confirm("Введите ваше им")) {
+                    var us_pnm1;
+                    var us_psr1;
+                    var us_pag1;
+                    us_pnm1 = prompt("Введиет своё им");
+                    if(us_pnm1 != usi_log1.us_usei1.us_usenm1 && us_pnm1 != usi_log1.us_usei1.us_usesr1) {
+                        us_psr1 = prompt("Введиет свою фамил");
+                        if(us_psr1 != usi_log1.us_usei1.us_usesr1 && us_psr1 != usi_log1.us_usei1.us_usenm1) {
+                            us_pag1 = prompt("Введиет свой возра");
+                            if(us_pag1 != usi_log1.us_usei1.us_useag1 && us_pag1 != usi_log1.us_usei1.us_usesr1) {
+                                if(confirm("Ваши данные сохране")) {
+                                    if(confirm("Можете теперь сделать свой i")) {
+                                        if(confirm("Эта функция плат")) {
+                                            Object.defineProperty(usi_log1, "us_usenm2", {value:us_pnm1, writable:true, enumerable:true, configurable:true});
+                                            if(usi_log1.hasOwnProperty("us_usenm2") == true && usi_log1.us_usenm2 == us_pnm1) {
+                                                if(confirm("Данные сохранены, но не все, можете сразу сделать свой i")) {
+                                                    if(confirm("Это платн")) {
+                                                        Object.defineProperty(usi_log1, "us_usesr2", {value:us_psr1, writable:true, enumerable:true, configurable:true});
+                                                        if(usi_log1.hasOwnProperty("us_usesr2") == true && usi_log1.us_usesr2 == us_psr1) {
+                                                            Object.defineProperty(usi_log1, "us_useag2", {value:us_pag1, writable:true, enumerable:true, configurable:true});
+                                                            if(usi_log1.hasOwnProperty("us_useag2") == true && usi_log1.us_useag2 == us_pag1) {
+                                                                if(confirm("Ваши данные сохоане")) {
+                                                                    if(confirm("Можете посмотреть свой бала")) {
+                                                                        if(confirm("Это тоже плат")) {
+                                                                            Object.defineProperty(usi_log1, "us_useba2", {value:"1199", writable:true, enumerable:true, configurable:true});
+                                                                            if(usi_log1.hasOwnProperty("us_useba2") == true && usi_log1.us_useba2 == "1199") {
+                                                                                if(confirm("Ваш балан" + " " + usi_log1.us_useba2)) {
+                                                                                    if(confirm("Можете переходить к функц")) {
+                                                                                        if(confirm("Но сначала заплатите за платные функ")) {
+                                                                                            if(confirm("Посмотреть дол")) {
+                                                                                                if(confirm("Долг в объек")) {
 
+                                                                                                } else {
+                                                                                                    if(confirm("Долг в масси")) {
+                                                                                                        var us_dhl2 = [us_lodo1.us_dlh1, us_lodo1.us_dlh2, us_lodo1.us_dlh3, us_lodo1.us_dlh4, us_lodo1.us_dlh5];
+                                                                                                        if(us_dhl2[0] != us_dhl2[1] && us_dhl2[1] != us_dhl2[3]) {
+                                                                                                            if(confirm("Надо его отсортирова")) {
+                                                                                                                if(confirm("Эта функция плат")) {
+                                                                                                                    us_dhl2.sort(" ");
+                                                                                                                    delete us_dhl2[1];
+                                                                                                                    us_dhl2.push("15");
+                                                                                                                    if(confirm("Ваш масс" + " " + us_dhl2)) {
+                                                                                                                        if(confirm("Посчитать автоматиче"))  {
+                                                                                                                            if(confirm("Посчита")) {
+                                                                                                                                
+                                                                                                                            }
+                                                                                                                        }
+                                                                                                                    }
+                                                                                                                }
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                if(confirm("В бесплатной фунцкции, не будет добавл")) {
+
+                }
+            }
+        }
     }
 }
